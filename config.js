ecb47f74b8eb3f5e97041d1190826ecc048213fb
Creative-WW <creative@wwdcomplete.com>
2025-08-19 15:32:23 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+
+window.projectConfig = {
+  title: "WW8549 . Pepsi Stacker Expansion",
+  description: "View the Pepsi Stacker Expansion in AR on your mobile device.",
+  image: "assets/Pepsi-Stacker-Expansion.jpg", // used for AR button
+  usdz: "assets/AR_WW8549_Pepsi.usdz",
+  glb: "assets/AR_WW8549_Pepsi.glb",
+  logo: "assets/dq_logo.png", // company logo
+  tapIcon: "assets/tap-icon.png" // dynamic tap icon
+};
