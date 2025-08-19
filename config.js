936889441139652a8902b45d313bbcb62c861a46
Creative-WW <creative@wwdcomplete.com>
2025-08-19 15:34:30 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // config.js
 
 window.projectConfig = {
-  title: "WW8549 . Pepsi Stacker Expansion",
-  description: "View the Pepsi Stacker Expansion in AR on your mobile device.",
-  image: "assets/Pepsi-Stacker-Expansion.jpg", // used for AR button
-  usdz: "assets/AR_WW8549_Pepsi.usdz",
-  glb: "assets/AR_WW8549_Pepsi.glb",
+  title: "DQ4287 . Advantage Endcap w Center Shelves",
+  description: "View the Advantage Endcap w Center Shelves in AR on your mobile device.",
+  image: "assets/Advantage-Endcap-Center-Shelves.jpg", // used for AR button
+  usdz: "assets/AR_DQ4287_Center Shelves.usdz",
+  glb: "assets/AR_DQ4287_Center Shelves.glb",
   logo: "assets/dq_logo.png", // company logo
   tapIcon: "assets/tap-icon.png" // dynamic tap icon
 };
